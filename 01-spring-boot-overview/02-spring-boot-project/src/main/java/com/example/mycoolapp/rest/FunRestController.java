9c4efba97d4a367666f938bedf6e864d570cb9f4
adan35 <adanshahzad35@gmail.com>
2024-10-23 14:34:57 +0500

    organized folder structure

---
+++ 01-spring-boot-overview/02-spring-boot-project/src/main/java/com/example/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,19 @@
+package com.example.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+    
+}
