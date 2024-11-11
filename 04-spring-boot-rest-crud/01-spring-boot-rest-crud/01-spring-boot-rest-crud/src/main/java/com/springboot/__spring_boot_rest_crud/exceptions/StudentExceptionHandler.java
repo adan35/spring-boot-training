3a3fcc2147449c2a525c4c4c420f7594c8a0d09d
adan35 <adanshahzad35@gmail.com>
2024-11-12 02:38:55 +0500

    GlobalExceptionHandler

---
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/springboot/__spring_boot_rest_crud/exceptions/StudentExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.springboot.__spring_boot_rest_crud.exceptions;
+
+import com.springboot.__spring_boot_rest_crud.dto.StudentErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class StudentExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setMessage(exc.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setMessage(exc.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+    
+}
